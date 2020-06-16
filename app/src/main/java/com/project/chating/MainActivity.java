8252e89cd126f8bc2118bdcc6d16b4d207b68767
diyanyagari <yagaripratamadiyan@gmail.com>
2020-06-16 23:51:41 +0700

    already done

--- app/src/main/java/com/project/chating/MainActivity.java
+++ app/src/main/java/com/project/chating/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,13 +21,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.firebase.ui.database.FirebaseListOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -42,13 +48,15 @@ public class MainActivity extends AppCompatActivity {
     private ListView listView;
     private String name;
     private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
-
+    private FirebaseListAdapter<ChatMessage> adapter;
     private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> list_of_rooms = new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
 
         if(FirebaseAuth.getInstance().getCurrentUser() == null) {
             startActivityForResult(
@@ -59,14 +67,34 @@ public class MainActivity extends AppCompatActivity {
             );
         } else {
             Toast.makeText(this,
-                    "Welcome " + FirebaseAuth.getInstance()
+                    "Selamat Datang " + FirebaseAuth.getInstance()
                             .getCurrentUser()
                             .getDisplayName(),
                     Toast.LENGTH_LONG)
                     .show();
-
-            // Load chat room contents
+            Log.d("tag", "tes");
             displayChatMessages();
+
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    EditText input = (EditText)findViewById(R.id.input);
+                    if(input.getText().toString().matches("")) {
+
+                    } else {
+                        FirebaseDatabase.getInstance()
+                                .getReference()
+                                .push()
+                                .setValue(new ChatMessage(input.getText().toString(),
+                                        FirebaseAuth.getInstance()
+                                                .getCurrentUser()
+                                                .getDisplayName())
+                                );
+
+                        input.setText("");
+                    }
+                }
+            });
         }
     }
 
@@ -75,24 +103,45 @@ public class MainActivity extends AppCompatActivity {
                                     Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText)findViewById(R.id.input);
+                if(input.getText().toString().matches("")) {
+
+                } else {
+                    FirebaseDatabase.getInstance()
+                            .getReference()
+                            .push()
+                            .setValue(new ChatMessage(input.getText().toString(),
+                                    FirebaseAuth.getInstance()
+                                            .getCurrentUser()
+                                            .getDisplayName())
+                            );
+
+                    input.setText("");
+                }
+            }
+        });
+
         if(requestCode == SIGN_IN_REQUEST_CODE) {
             if(resultCode == RESULT_OK) {
                 Toast.makeText(this,
-                        "Successfully signed in. Welcome!",
+                        "Berhasil Masuk. Selamat Datang!",
                         Toast.LENGTH_LONG)
                         .show();
                 displayChatMessages();
             } else {
                 Toast.makeText(this,
-                        "We couldn't sign you in. Please try again later.",
+                        "Gagal Masuk. Silahkan Coba Lagi.",
                         Toast.LENGTH_LONG)
                         .show();
-
-                // Close the app
                 finish();
             }
         }
-
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -107,11 +156,9 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             Toast.makeText(MainActivity.this,
-                                    "You have been signed out.",
+                                    "Anda Berhasil Keluar",
                                     Toast.LENGTH_LONG)
                                     .show();
-
-                            // Close activity
                             finish();
                         }
                     });
@@ -119,6 +166,41 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
     private void displayChatMessages() {
+        ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
 
+        Query query = FirebaseDatabase.getInstance().getReference();
+
+        FirebaseListOptions<ChatMessage> options = new FirebaseListOptions.Builder<ChatMessage>()
+                .setQuery(query, ChatMessage.class)
+                .setLayout(R.layout.message)
+                .build();
+        adapter = new FirebaseListAdapter<ChatMessage>(options) {
+            @Override
+            protected void populateView(@NonNull View v, @NonNull ChatMessage model, int position) {
+                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                        model.getMessageTime()));
+            }
+        };
+        listOfMessages.setAdapter(adapter);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        adapter.stopListening();
+//    }
 }
